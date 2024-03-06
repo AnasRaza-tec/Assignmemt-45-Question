ec82f0781ad8d7c074ec673a3fa8fefc57b8550a
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:22:13 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
-console.log(6 + 2);
-console.log(20 - 12);
-console.log(2 * 4);
-console.log(32 / 4);
+var FavoriteNumber = 7;
+console.log("My Favorite Number ".concat(FavoriteNumber));
--- app.ts
+++ app.ts
@@ -1,4 +1,2 @@
-console.log(6+2);
-console.log(20-12);
-console.log(2*4);
-console.log(32/4);
+let FavoriteNumber : number = 7 ;
+console.log(`My Favorite Number ${FavoriteNumber}`);
