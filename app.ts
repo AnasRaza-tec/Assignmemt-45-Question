aac51c881ce46a23020f6990f7cacf500ec5ec32
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:27:02 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,16 +1,6 @@
-let guest : string [] = ["janan", "zain", "saad", "hair"];
-let newguest : string = ("jola");
-let notattandguest : string = ("saad");
-guest.unshift("imran", "arman");
-guest[4]= newguest;
-console.log(`mr ${notattandguest} sorry you not attend diner `);
-console.log("Sorry i am invited 2 people.")
-while(guest.length>2){
-let remove_guest = guest.pop()
-console.log(`Assamualikum ${ remove_guest } do you not invited`);   
-}
-for (let R=0; R<guest.length; R++){
-console.log ("Assalamualikum " + guest[R] + " you will be invite to wedding.\n\nThank You\n")    
-}
-guest.splice(0,2);
-console.log(guest);
+let places : string [] =  [ "karachi", "lahore", "rawalpindi", "islamabad","sawat"];
+console.log(`orignal `+ places);
+console.log(`copy ` + [...places].sort());
+console.log(`orignal `+ places);
+console.log(`copy `+ [...places].reverse());
+console.log(`orignal `+ places);
