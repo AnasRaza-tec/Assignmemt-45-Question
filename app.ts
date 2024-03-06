ce4402f939872236fab47c8129a572976223bb0e
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:29:38 +0500

    Add files via upload

--- app.ts
+++ app.ts
@@ -1,8 +1,28 @@
-let aray : (string | number)[] = ["anas", "janan", "zain", "sadd", 1, 2, 3,];
-console.log( aray[0]);
-console.log( aray[1]);
-console.log( aray[2]);
-console.log( aray[3]);
-console.log( aray[4]);
-console.log( aray[5]);
-console.log( aray[6]);
+let person_name = "Anas Raza"
+// Text 1
+console.log("Is person_name == 'Anas Raza'? I predict True.");
+console.log(person_name == "Anas Raza");
+// Text 2
+console.log("Is person_name === 'Anas Raza'? I predict True.");
+console.log(person_name === "Anas Raza");
+// Text 3 
+console.log("Is person_name != 'Anas Raza'? I predict false.");
+console.log(person_name != "Anas Raza");
+// Text 4
+console.log("Is person_name !== 'Anas Raza'? I predict false.");
+console.log(person_name !== "Anas Raza");
+// Text 5
+console.log("Is person_name < 'Anas Raza'? I predict false.");
+console.log(person_name < "Anas Raza");
+// Text 6
+console.log("Is person_name > 'Anas Raza'? I predict false.");
+console.log(person_name > "Anas Raza");
+// Text 7
+console.log("Is person_name <= 'Anas Raza'? I predict True.");
+console.log(person_name <= "Anas Raza");
+// Text 8
+console.log("Is person_name >= 'Anas Raza'? I predict True.");
+console.log(person_name >= "Anas Raza");
+// Text 9
+console.log("Is person_name 'Anas Raza'? I predict True.");
+console.log(person_name);
