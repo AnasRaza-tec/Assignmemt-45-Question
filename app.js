ccacfa1910187d8675c1e4b5e0dbe0affadecfc7
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:26:03 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var guestarr = ["janan", "zain", "saad", "haris"];
-var haris = "haris";
-console.log("".concat(haris, " can not be invited to diner"));
-var jola = "Imran";
+var haris = ("haris");
+var jola = ("jola");
 guestarr[guestarr.indexOf(haris)] = jola;
+//console.log(guestarr);
+//guestarr.map((items) => console.log(`Dear ${items}, I found a bigger dinner table so i am invited more peples.`));
+// Part Tow
+var newone = ("inram");
+guestarr.unshift(newone);
+//console.log(guestarr);
+var secondone = ("arman");
+var middleIndex = guestarr.length / 2;
+guestarr.splice(middleIndex, 0, secondone);
+//console.log(guestarr);
+guestarr.push("anas");
 console.log(guestarr);
-guestarr.map(function (items) { return console.log("Dear ".concat(items, " , can be invite to diner")); });
+//guestarr.map((items) => console.log(`Dear ${items}, I found a bigger dinner table so i am invited more peples.`));
--- app.ts
+++ app.ts
@@ -1,7 +1,17 @@
- let guestarr : string[] = ["janan","zain","saad","haris"];
- let haris : string = "haris"
-console.log(`${haris} can not be invited to diner`);
- let jola : string = "Imran"
-guestarr [guestarr.indexOf(haris)] = jola ; 
-console.log(guestarr); 
-guestarr.map((items) => console.log(`Dear ${items} , can be invite to diner`));
+let guestarr : string[] = ["janan", "zain", "saad","haris"];
+let haris :string = ("haris");
+let jola :string = ("jola");
+guestarr [guestarr.indexOf(haris)] = jola ;
+//console.log(guestarr);
+//guestarr.map((items) => console.log(`Dear ${items}, I found a bigger dinner table so i am invited more peples.`));
+// Part Tow
+let newone : string = ("inram");
+guestarr.unshift(newone);
+//console.log(guestarr);
+let secondone : string = ("arman");
+let middleIndex = guestarr.length/2;
+guestarr.splice (middleIndex,0,secondone );
+//console.log(guestarr);
+guestarr.push("anas");
+//console.log(guestarr);
+guestarr.map((items) => console.log(`Dear ${items}, I found a bigger dinner table so i am invited more peples.`));
