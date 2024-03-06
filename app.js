3928cffaaa81bd2d573289f2a7df11fdfbad0c4b
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:29:04 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-var toyota = {
-    car: "toyota supra",
-    price: 120000.00,
-};
-console.log("car name ".concat(toyota.car, " \n price ").concat(toyota.price, "\n"));
+var aray = ["anas", "janan", "zain", "sadd", 1, 2, 3,];
+console.log(aray[0]);
+console.log(aray[1]);
+console.log(aray[2]);
+console.log(aray[3]);
+console.log(aray[4]);
+console.log(aray[5]);
+console.log(aray[6]);
--- app.ts
+++ app.ts
@@ -1,9 +1,8 @@
-interface items{
-car : string
-price : number    
-}
-const toyota : items={
-car: "toyota supra",
-price: 120000.00, 
-}
-console.log(`car name ${toyota.car} \n price ${toyota.price}\n`);
+let aray : (string | number)[] = ["anas", "janan", "zain", "sadd", 1, 2, 3,];
+console.log( aray[0]);
+console.log( aray[1]);
+console.log( aray[2]);
+console.log( aray[3]);
+console.log( aray[4]);
+console.log( aray[5]);
+console.log( aray[6]);
