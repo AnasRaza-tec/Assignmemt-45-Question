66bf054709aa272c35fa4156625167f4f408301c
AnasRaza-tec <157498271+AnasRaza-tec@users.noreply.github.com>
2024-03-06 20:20:34 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-var quote = "A person who never made a mistake never tried anything new.";
-var author = "Albert Einstein";
-console.log("".concat(author, " once said, ").concat(quote));
+var quote = " A person who never made a mistake never tried anything new . ";
+var author = " Albert Einstein ";
+var massege = "".concat(author, " once said,  ").concat(quote);
+console.log(massege);
